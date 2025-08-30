df56d6a97addbc2cacb3f526b7994b14116c197d
Caceresenzo <caceresenzo1502@gmail.com>
2025-08-30 15:49:44 +0200

    pass 1st file-search

--- app/main.py
+++ app/main.py
@@ -1,26 +1,52 @@
 import sys
 
+import click
+
 from app.regex import Pattern
 
 
-def main():
+@click.command()
+@click.option("-E", "--extended-regexp", is_flag=True, help="Use extended regular expressions")
+@click.argument("expression")
+@click.argument("files", nargs=-1)
+def main(
+    extended_regexp: bool,
+    expression: str,
+    files: list[str],
+):
+    if not extended_regexp:
+        print("grep: -E must be used")
+        exit(1)
+
     expression = sys.argv[2]
-    input_line = sys.stdin.read()
-
-    if sys.argv[1] != "-E":
-        print("Expected first argument to be '-E'")
-        exit(1)
-
     pattern = Pattern.compile(expression)
-    matcher = pattern.matcher(input_line)
 
-    if matcher.find(0):
-        for number in range(matcher.group_count() + 1):
-            print(f"group[{number}] = `{matcher.group(number)}`")
+    found = False
 
-        exit(0)
+    if not len(files):
+        found |= handle_line(pattern, sys.stdin.read())
+
     else:
+        for file in files:
+            try:
+                with open(file, "r") as fd:
+                    for line in fd:
+                        found |= handle_line(pattern, line.strip())
+            except FileNotFoundError:
+                print(f"grep: {file}: No such file or directory", file=sys.stderr)
+
+    if not found:
         exit(1)
+
+
+def handle_line(pattern: Pattern, line: str) -> bool:
+    matcher = pattern.matcher(line)
+
+    if not matcher.find(0):
+        return False
+
+    print(line)
+    return True
 
 
 if __name__ == "__main__":
